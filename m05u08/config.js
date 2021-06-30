3ca2e10d148f23d64f91faa26e327ac8e795a943
MP <mpolicht@hotmail.com>
2021-06-30 13:45:28 -0400

    m0508

---
+++ m05u08/config.js
@@ -0,0 +1,8 @@
+const config = {};
+
+config.endpoint = "COSMOS_DB_SQL_API_ENDPOINT";
+config.key = "COSMOS_DB_ACCESS_KEY";
+config.database = "iotdb";
+config.container = "iotcollection";
+
+module.exports = config;
