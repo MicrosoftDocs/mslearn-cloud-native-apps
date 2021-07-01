c69da4f6b60848b49781b8ee128871c400cbdf93
MP <mpolicht@hotmail.com>
2021-07-01 08:21:19 -0400

    m06u07

---
+++ m06u07/src/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { AuthenticatedTemplate, UnauthenticatedTemplate, useMsal } from "@azure/msal-react";
+import { loginRequest } from "./authConfig";
+import { PageLayout } from "./components/PageLayout";
+import { ProfileData } from "./components/ProfileData";
+import { callMsGraph } from "./graph";
+import Button from "react-bootstrap/Button";
+import "./styles/App.css";
+
+var accessToken = '';
+
+/**
+ * Renders information about the signed-in user or a button to retrieve data about the user
+ */
+const ProfileContent = () => {
+    const { instance, accounts } = useMsal();
+    const [graphData, setGraphData] = useState(null);
+
+    function RequestProfileData() {
+        // Silently acquires an access token which is then attached to a request for MS Graph data
+        instance.acquireTokenSilent({
+            ...loginRequest,
+            account: accounts[0]
+        }).then((response) => {
+            accessToken = response.accessToken;
+            callMsGraph(response.accessToken).then(response => setGraphData(response));
+        });
+    }
+
+    return (
+        <>
+            <h5 className="card-title">Welcome {accounts[0].name}</h5>
+            {graphData ? 
+                <ProfileData graphData={graphData} />
+                :
+                <Button variant="secondary" onClick={RequestProfileData}>Request Profile Information</Button>
+            }
+            <p>{accessToken}</p>
+        </>
+    );
+};
+
+/**
+ * If a user is authenticated the ProfileContent component above is rendered. Otherwise a message indicating a user is not authenticated is rendered.
+ */
+const MainContent = () => {    
+    return (
+        <div className="App">
+            <AuthenticatedTemplate>
+                <ProfileContent />
+            </AuthenticatedTemplate>
+
+            <UnauthenticatedTemplate>
+                <h5 className="card-title">Please sign-in to see your profile information.</h5>
+            </UnauthenticatedTemplate>
+        </div>
+    );
+};
+
+export default function App() {
+    return (
+        <PageLayout>
+            <MainContent />
+        </PageLayout>
+    );
+}
